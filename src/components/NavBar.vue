9f580477e7b7b018ae36e14b46a2f47e9ca13f47
owentr1369 <tamvaa1306@gmail.com>
2022-06-12 22:58:42 +0700

    home card template

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,11 +3,9 @@
     <v-badge color="#D5F0DB" dot>
       <v-img src="../assets/1.png" contain width="30"></v-img>
     </v-badge>
-
     <v-toolbar-title class="ml-4">
       <span class="green--text">e</span><strong>Grocery</strong>
     </v-toolbar-title>
-
     <v-spacer></v-spacer>
     <v-badge color="#FF6D59" overlap content="2" class="mr-2 mt-1">
       <v-avatar color="#FFF0EE" size="40">
@@ -35,4 +33,8 @@
 export default {};
 </script>
 
-<style></style>
+<style>
+.v-toolbar__title {
+  font-size: 1rem !important;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,71 @@
 <template>
   <v-app>
     <NavBar />
-    <v-container fluid></v-container>
+    <v-container fluid>
+      <v-row>
+        <v-col cols="12">
+          <v-card class="mx-auto rounded-xl" flat>
+            <v-img
+              src="../assets/cover2.png"
+              max-height="100%"
+              max-width="100%"
+              contain
+            >
+              <v-card-title class="top ml-10">
+                <h2 class="title1">Order your</h2>
+              </v-card-title>
+              <v-card-title class="ml-10 mt-n8">
+                <h2 class="title2">Daily Groceries</h2>
+              </v-card-title>
+              <v-card-text class="ml-10">
+                <h3 class="green--text">#Free Delivery</h3>
+              </v-card-text>
+              <v-card-text class="ml-10">
+                <v-row>
+                  <v-col cols="4">
+                    <v-text-field
+                      label="Search your daily groceries"
+                      class="d-none d-sm-none d-md-flex"
+                      filled
+                      prepend-inner-icon="mdi-magnify"
+                      solo
+                      flat
+                      background-color=""
+                      rounded
+                      outlined
+                      success
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="2" class="marginLeft">
+                    <v-btn
+                      color="green"
+                      rounded
+                      dark
+                      x-large
+                      class="py-7 d-none d-sm-none d-md-flex"
+                    >
+                      search
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-img>
+          </v-card>
+          <v-toolbar flat color="transparent">
+            <v-toolbar-title class="text-h6">Category</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn-toggle v-model="toggle_exclusive" group color="#49D9A0">
+              <v-btn>
+                <v-icon>mdi-chevron-left</v-icon>
+              </v-btn>
+              <v-btn>
+                <v-icon>mdi-chevron-right</v-icon>
+              </v-btn>
+            </v-btn-toggle>
+          </v-toolbar>
+        </v-col></v-row
+      >
+    </v-container>
   </v-app>
 </template>
 
@@ -14,3 +78,48 @@ export default {
   },
 };
 </script>
+
+<style>
+.marginLeft {
+  margin-left: -90px;
+}
+.mtop {
+  margin-top: 100px;
+}
+.mbottom {
+  margin-bottom: 100px;
+}
+.v-card.borderme {
+  border: 2px solid green !important;
+}
+.v-card.borderout {
+  border: 1px solid #d5f0db !important;
+}
+.v-btn:not(.v-btn--round).v-size--default.add {
+  min-width: 0px !important;
+}
+.theme--light.v-sheet--outlined.mobile {
+  border: 2px solid black !important;
+}
+@media only screen and (max-width: 600px) {
+  h2.title1 {
+    font-size: 15px;
+  }
+  h2.title2 {
+    font-size: 15px;
+  }
+  .top {
+    margin-top: 20px;
+  }
+}
+@media only screen and (min-width: 600px) {
+  .top {
+    margin-top: 70px;
+  }
+}
+@media only screen and (min-width: 768px) {
+  .top {
+    margin-top: 120px;
+  }
+}
+</style>
